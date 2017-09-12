1e433a65b4f850596611a3aaed6242dbd21c3d47
Raymond <Raymond.octavious@ocsbstudent.ca>
2017-09-12 13:30:52 -0400

    Initial Comment

---
+++ hello_world_gui.py
@@ -0,0 +1,9 @@
+# Created by Raymond Octavious
+# Created on September 12 2017
+# Created for class ICS3U
+# This is displays "Hello World" in a GUI
+
+import ui
+
+v = ui.load_view()
+v.present('sheet')
